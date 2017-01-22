f488d7274a9316311384f343001eb0c2bc77f1a8
Silviu <Silviu>
2017-01-22 18:43:11 +0200

    Course 9 exercise 5.6

---
+++ Course8/CustomAttributes/MaxWordsAttributes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Course8.CustomAttributes
+{
+    public class MaxWordsAttributes:ValidationAttribute 
+
+    {
+        private readonly int _maxwords;
+
+            public MaxWordsAttributes(int maxWords):base("{0} has too many words.")
+            {
+            _maxwords = maxWords;
+        }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if(value!=null)
+            {
+                var valueAsString = value.ToString();
+                if (valueAsString.Split(' ').Length > _maxwords)
+                {
+                    var errorMessage = FormatErrorMessage(validationContext.DisplayName);
+                    return new ValidationResult(errorMessage);
+                }
+                     
+                }
+                return ValidationResult.Success;
+            }
+        }
+    }
--- Course8/Models/ProjectEvaluationModels.cs
+++ Course8/Models/ProjectEvaluationModels.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Course8.CustomAttributes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -8,16 +9,21 @@ using System.Web.Mvc;
 namespace Course8.Models
 {
     [Bind(Exclude = "Country")]
-    public class ProjectEvaluationModels
+    public class ProjectEvaluationModels:IValidatableObject
     {
         public int Id { get; set; }
         [StringLength(10)]
         public string Name { get; set; }
+        [MaxWordsAttributes(2)]
         public string City { get; set; }
         public string Country { get; set; }
         [Required,Range(1,10)]        
         public double Rating { get; set; }
 
-
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(Rating<2 && Name.ToLower().StartsWith("john"))
+            { yield return new ValidationResult("Sorry John ,you can't do this"); }
+        }
     }
 }
